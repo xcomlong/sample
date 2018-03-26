a7570236813192def4b2887d227b3a65d6b96bdd
xcomlong <330415508@qq.com>
2018-03-26 09:36:55 +0800

    Add files via upload

---
+++ dialog-transform-demo/static/testData.js
@@ -0,0 +1,135 @@
+export let TestData1 = [{
+    id: 1,
+    label: "HTML5",
+    checked: false,
+    required: false,
+    type:'From',
+    classType:[],
+    from:[],
+    to:[],
+    columnIndex:0
+  },
+  {
+    id: 2,
+    label: "CSS3",
+    checked: false,
+    required: true,
+    type:'From',
+    classType:[],
+    from:[],
+    to:[],
+    columnIndex:0
+  },
+  {
+    id: 3,
+    label: "Angular",
+    checked: false,
+    required: false,
+    type:'From',
+    classType:[],
+    from:[],
+    to:[],
+    columnIndex:0
+  },
+  {
+    id: 4,
+    label: "Vue",
+    checked: false,
+    required: true,
+    type:'From',
+    classType:[],
+    from:[],
+    to:[],
+    columnIndex:0
+  },
+  {
+    id: 5,
+    label: "Linux",
+    checked: false,
+    required: false,
+    type:'From',
+    classType:[],
+    from:[],
+    to:[],
+    columnIndex:0
+  },
+  {
+    id: 6,
+    label: "JavaScript",
+    checked: false,
+    required: true,
+    type:'From',
+    classType:[],
+    from:[],
+    to:[],
+    columnIndex:0
+  }
+];
+
+export let TestData2 =  [{
+    id: 1,
+    label: "HTML5",
+    checked: false,
+    required: false,
+    type:'To',
+    classType:[],
+    from:[],
+    to:[],
+    columnIndex:0
+  },
+  {
+    id: 2,
+    label: "CSS3",
+    checked: false,
+    required: true,
+    type:'To',
+    classType:[],
+    from:[],
+    to:[],
+    columnIndex:0
+  },
+  {
+    id: 3,
+    label: "Angular",
+    checked: false,
+    required: false,
+    type:'To',
+    classType:[],
+    from:[],
+    to:[],
+    columnIndex:0
+  },
+  {
+    id: 4,
+    label: "Vue",
+    checked: false,
+    required: true,
+    type:'To',
+    classType:[],
+    from:[],
+    to:[],
+    columnIndex:0
+  },
+  {
+    id: 5,
+    label: "Linux",
+    checked: false,
+    required: false,
+    type:'To',
+    classType:[],
+    from:[],
+    to:[],
+    columnIndex:0
+  },
+  {
+    id: 6,
+    label: "JavaScript",
+    checked: false,
+    required: true,
+    type:'To',
+    classType:[],
+    from:[],
+    to:[],
+    columnIndex:0
+  }
+];
